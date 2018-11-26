fdfdc96ff0283d87d2ebb00070f6960b1c1c5428
lanjiessh <lanjiessh@sina.com>
2018-11-26 09:00:42 +0800

    init

---
+++ app/src/main/java/com/lanjiessh/baselib/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lanjiessh.baselib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
